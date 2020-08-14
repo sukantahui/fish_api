98f40325dfd574a29a2187734f132e533b5eb795
Sreeparna Das <sreeparna@grmtech.com>
2020-08-14 23:07:04 +0530

    updated

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//updated by sreeparna
+
 Route::post('/register', 'AuthController@register');
 
 Route::post('/login', 'AuthController@login');
