a0f5d7c488140de0e2dda12eb89467688f9ac7fb
sukanta hui <huisukanta77@gmail.com>
2020-08-25 21:04:45 +0530

    Changed

--- app/Model/Ledger.php
+++ app/Model/Ledger.php
@@ -7,4 +7,64 @@ use Illuminate\Database\Eloquent\Model;
 class Ledger extends Model
 {
     //
+    /**
+     * @var mixed
+     */
+    private $ledger_name;
+    /**
+     * @var mixed
+     */
+    private $billing_name;
+    /**
+     * @var mixed
+     */
+    private $ledger_group_id;
+    /**
+     * @var mixed
+     */
+    private $email;
+    /**
+     * @var mixed
+     */
+    private $mobile1;
+    /**
+     * @var mixed
+     */
+    private $mobile2;
+    /**
+     * @var mixed
+     */
+    private $address1;
+    /**
+     * @var mixed
+     */
+    private $address2;
+    /**
+     * @var mixed
+     */
+    private $state;
+    /**
+     * @var mixed
+     */
+    private $po;
+    /**
+     * @var mixed
+     */
+    private $area;
+    /**
+     * @var mixed
+     */
+    private $city;
+    /**
+     * @var mixed
+     */
+    private $pin;
+    /**
+     * @var mixed
+     */
+    private $opening_balance;
+    /**
+     * @var mixed
+     */
+    private $transaction_type_id;
 }
--- app/Model/purchaseMaster.php
+++ app/Model/purchaseMaster.php
@@ -14,14 +14,14 @@ class purchaseMaster extends Model
         "inforced","created_at","updated_at"
     ];
 
-    public function vendor()
-    {
-        return $this->belongsTo('App\User','vendor_id');
-    }
-    public function employee()
-    {
-        return $this->belongsTo('App\User','employee_id');
-    }
+//    public function vendor()
+//    {
+//        return $this->belongsTo('App\Model\Ledger','vendor_id');
+//    }
+//    public function employee()
+//    {
+//        return $this->belongsTo('App\User','employee_id');
+//    }
     public function purchaseDetails(){
         return $this->hasMany('App\Model\PurchaseDetail','purchase_master_id');
     }
