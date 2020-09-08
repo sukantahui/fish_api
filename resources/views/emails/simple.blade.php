2f4460c9aab396e9e9b5c107ca7b047d09e8e9cf
sukanta hui <huisukanta77@gmail.com>
2020-09-08 10:43:22 +0530

    mail added with picture

--- resources/views/emails/simple.blade.php
+++ resources/views/emails/simple.blade.php
@@ -1,2 +1,6 @@
 Hello <strong>{{ $name }}</strong>,
 <p>{{$body}}</p>
+
+<a target="_blank" href="{{ url('/') }}/images/fish1.jpg">
+    <img src="{{ $message->embed(public_path('images/fish1.jpg')) }}" width="200px">
+</a>
