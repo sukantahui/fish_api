6dd0a1892bec46a22e60321e628ba5b7479937a9
sukanta hui <huisukanta77@gmail.com>
2020-09-10 21:41:45 +0530

    sale total function added

--- database/migrations/3020_05_11_152837_create_all_procedures_and_functions.php
+++ database/migrations/3020_05_11_152837_create_all_procedures_and_functions.php
@@ -38,6 +38,26 @@ class CreateAllProceduresAndFunctions extends Migration
                 RETURN total_bill_amount;
             END'
         );
+        DB::unprepared('DROP FUNCTION IF EXISTS get_sale_amount_by_transaction_master_id;
+            CREATE FUNCTION get_sale_amount_by_transaction_master_id (temp_transaction_master_id bigint) RETURNS double
+            READS SQL DATA
+            DETERMINISTIC
+            BEGIN
+                DECLARE temp_sale_master_id double;
+                DECLARE total_bill_amount double;
+                set total_bill_amount=0;
+                select sale_master_id into temp_sale_master_id from transaction_masters where id=temp_transaction_master_id;
+
+                IF isnull(temp_sale_master_id) then
+                    set total_bill_amount=0;
+                    RETURN total_bill_amount;
+                END IF;
+                select sum(quantity*price-discount)  into total_bill_amount from sale_details where sale_master_id = temp_sale_master_id;
+                select total_bill_amount-discount-round_off+loading_n_unloading_expenditure  into total_bill_amount from sale_masters where id = temp_sale_master_id;
+
+                RETURN total_bill_amount;
+            END'
+        );
     }
 
     /**
