35d4ea5000c4ced6c0c5f1184ccb37f96ca07500
Sreeparna Das <sreeparnadas0675@gmail.com>
2020-09-28 21:04:26 +0530

    adding container model

---
+++ app/Http/Controllers/ContainerTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\ContainerType;
+use Illuminate\Http\Request;
+
+class ContainerTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\ContainerType  $containerType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ContainerType $containerType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\ContainerType  $containerType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ContainerType $containerType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\ContainerType  $containerType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ContainerType $containerType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\ContainerType  $containerType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ContainerType $containerType)
+    {
+        //
+    }
+}
